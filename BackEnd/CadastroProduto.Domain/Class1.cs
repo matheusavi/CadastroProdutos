c084ae3259a7d506b1694eb06da5c0fe8493d640
spyker_0 <matheusavi.47@gmail.com>
2020-07-09 22:10:14 -0300

    commit inicial

---
+++ BackEnd/CadastroProduto.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CadastroProduto.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ BackEnd/CadastroProduto.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CadastroProduto.Infra
+{
+    public class Class1
+    {
+    }
+}
